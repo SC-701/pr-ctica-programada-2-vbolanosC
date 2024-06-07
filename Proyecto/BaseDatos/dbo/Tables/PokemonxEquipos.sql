efb58080a03a0a385ce072b3a95d26be9e7cd87e
Drojascode <drojas30534@ufide.ac.cr>
2024-06-07 01:00:51 -0600

    Se crea el repositorio base

---
+++ Proyecto/BaseDatos/dbo/Tables/PokemonxEquipos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PokemonxEquipos] (
+    [IdPokemon] UNIQUEIDENTIFIER NOT NULL,
+    [IdEquipo]  UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_PokemonxEquipos] PRIMARY KEY CLUSTERED ([IdPokemon] ASC, [IdEquipo] ASC),
+    CONSTRAINT [FK_PokemonxEquipos_Equipos] FOREIGN KEY ([IdEquipo]) REFERENCES [dbo].[Equipos] ([IdEquipo]) ON UPDATE CASCADE,
+    CONSTRAINT [FK_PokemonxEquipos_Pokemon] FOREIGN KEY ([IdPokemon]) REFERENCES [dbo].[Pokemon] ([Id]) ON UPDATE CASCADE
+);
+
